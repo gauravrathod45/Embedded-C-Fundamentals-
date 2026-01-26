1796c31b1856508f33986d28e56595d990cdeb46
Gaurav Rathod <gauravrathodathod7@gmail.com>
2026-01-26 09:56:00 +0530

    Added function example program

---
+++ function_example.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Function declaration */
+int add(int a, int b);
+
+int main() {
+    int num1, num2, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    /* Function call */
+    result = add(num1, num2);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
+
+/* Function definition */
+int add(int a, int b) {
+    return a + b;
+}
